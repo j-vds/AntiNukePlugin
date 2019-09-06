b620694174b8e3aff4821c83dc4f35a98713b4eb
J-VdS <jeroen.vanderstraeten@ugent.be>
2019-09-06 16:03:45 +0200

    try catch

--- src/main/java/reactorplugin/AntiNukePlugin.java
+++ src/main/java/reactorplugin/AntiNukePlugin.java
@@ -20,20 +20,24 @@ public class AntiNukePlugin extends Plugin {
     public AntiNukePlugin() {
         //listen for a block selection event
         Events.on(BuildSelectEvent.class, event -> {
-            if (!event.breaking && event.builder.buildRequest().block == Blocks.thoriumReactor && event.builder instanceof Player) {
-                //send a message to everyone saying that this player has begun building a reactor
-                Tile coreTile = ((Player) event.builder).getClosestCore().getTile();
-                if (coreTile == null) {
-                    return;
-                }
+            try {
+                if (!event.breaking && event.builder.buildRequest().block == Blocks.thoriumReactor && event.builder instanceof Player) {
+                    //send a message to everyone saying that this player has begun building a reactor
+                    Tile coreTile = ((Player) event.builder).getClosestCore().getTile();
+                    if (coreTile == null) {
+                        return;
+                    }
 
-                if (Math.sqrt(Math.pow(coreTile.x - event.tile.x, 2) + Math.pow(coreTile.y - event.tile.y, 2)) < (float) radius) {
-                    ((Player) event.builder).sendMessage("[scarlet] TOO CLOSE TO THE CORE, STOP");
-                    Call.beginBreak(event.builder.getTeam(), event.tile.x, event.tile.y);
-                    Call.onDeconstructFinish(event.tile, Blocks.thoriumReactor);
-                    Log.info("Player {0} tried to nuke the core...", ((Player) event.builder).name);
-                    return;
+                    if (Math.sqrt(Math.pow(coreTile.x - event.tile.x, 2) + Math.pow(coreTile.y - event.tile.y, 2)) < (float) radius) {
+                        ((Player) event.builder).sendMessage("[scarlet] TOO CLOSE TO THE CORE, STOP");
+                        Call.beginBreak(event.builder.getTeam(), event.tile.x, event.tile.y);
+                        Call.onDeconstructFinish(event.tile, Blocks.thoriumReactor);
+                        Log.info("Player {0} tried to nuke the core...", ((Player) event.builder).name);
+                        return;
+                    }
                 }
+            } catch (Exception e) {
+                Log.err("Nuke spam detected...");
             }
         });
     }
